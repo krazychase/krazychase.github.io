c200e883fd944cdd1f9f6d4fa774e4fec33d1b96
Chase Brown <krazychase@gmail.com>
2023-03-07 13:41:57 -0700

    Added React page

---
+++ src/components/ContactUs.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+export default class ContactUs extends Component {
+  render() {
+    let resumeData = this.props.resumeData;
+    return (
+      <section id="contact">
+          <div className="row section-head">
+            <div className="ten columns">
+              <p className="lead">
+              Feel free to contact me!
+              </p>
+            </div>
+          </div>
+        </section>
+    );
+  }
+}
---
+++ src/resumeData.js
@@ -0,0 +1,107 @@
+let resumeData = {
+    "imagebaseurl":"https://krazychase.github.io",
+    "name": "Chase Brown",
+    "role": "Computer Scientist / Software Engineer / Cybersecurity Enthusiast",
+    "roleDescription": "",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/chase-brown-1b1b0a245/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/krazychase",
+          "className":"fa fa-github"
+        }
+      ],
+    "aboutme":"I am an innovative Computer Scientist who learns quickly and embraces challenges. I seek to obtain a full-time opportunity that will allow me to use/expand my knowledge of software engineering, cybersecurity, and new technologies. I hope to learn, grow, and experience a myriad of new things.",
+    "address":"Western Colorado, United States",
+    "website":"https://krazychase.github.io",
+    "education":[
+      {
+        "UniversityName":"Colorado Mesa University",
+        "specialization":"Bachelor of Science in Computer Science",
+        "MonthOfPassing":"March",
+        "YearOfPassing":"2021",
+        "Achievements":"Cybersecurity Minor / Magna Cum Laude / 3.91 GPA"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Walker Products Inc.",
+        "specialization":"Software Engineer",
+        "MonthOfLeaving":"Current",
+        "YearOfLeaving":"",
+        "Achievements":"Design, develop, manage, and maintain automotive testing software using Python, C++, and SQL from scratch. Develop compact, secure code for microcontrollers using C/C++. Integrate and deploy software components into a fully functional hardware test system using Git. Produce specifications and determine operational feasibility of given hardware. Maintained, managed, and operated 3D printers and accompanying software. Communicate and collaborate among fellow engineers and production employees to attain a common goal."
+      },
+      {
+        "CompanyName":"NASA Colorado Space Grant Consortium",
+        "specialization":"Intern",
+        "MonthOfLeaving":"July",
+        "YearOfLeaving":"2019",
+        "Achievements":"Build and maintain rapid prototyping machines such as 3D printers, CNC machines, and CO2 laser cutters. Assist in development of JavaScript program to interact with hardware."
+      }
+    ],
+    "skillsDescription":"Languages / Frameworks",
+    "skills":[
+      {
+        "skillname":"Python",
+        "level":"_90"
+      },
+      {
+        "skillname":"C++",
+        "level":"_60"
+      },
+      {
+        "skillname":"JavaScript / ReactJS / TypeScript",
+        "level":"_50"
+      },
+      {
+        "skillname":"SQL",
+        "level":"_70"
+      },
+      {
+        "skillname":"Git / GitHub",
+        "level":"_70"
+      },
+      {
+        "skillname":"Agile / Scrum",
+        "level":"_90"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/coffee.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/origami.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/retrocam.jpg"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/console.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
